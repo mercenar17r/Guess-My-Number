991a2e6457367be1fc38f8d4f0aac87514b9512b
Negrut Cosmin <cosminnegrut776@gmail.com>
2024-09-07 03:42:50 +0300

    generate number and display message for not a number

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 'use strict';
 
-// document.getElementsByClassName('.message');
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct Number';
-// document.querySelector('.number').textContent = 7;
-// document.querySelector('.score').textContent = 10;
-// document.querySelector('.guess').value = 17;
-// console.log(document.querySelector('.guess').value);
+document.getElementsByClassName('.message');
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+document.querySelector('.number').textContent = 7;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 17;
+console.log(document.querySelector('.guess').value);
+
+const number = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!🙅‍♂️';
+  }
 });
