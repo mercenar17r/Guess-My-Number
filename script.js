c60e782a3974c3d57ead4d82747f88d8a22a7fd3
Negrut Cosmin <cosminnegrut776@gmail.com>
2024-09-07 03:34:41 +0300

    add event handler and store it

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 'use strict';
 
-document.getElementsByClassName('.message');
+// document.getElementsByClassName('.message');
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number';
+// document.querySelector('.number').textContent = 7;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 17;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+});
