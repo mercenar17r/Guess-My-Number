8f6d89d4a5817fd28af6d319937cee057dd61c36
Negrut Cosmin <cosminnegrut776@gmail.com>
2024-09-08 23:43:24 +0300

    clean code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-document.getElementsByClassName('.message');
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number';
-document.querySelector('.number').textContent = 7;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 17;
-console.log(document.querySelector('.guess').value);
+// document.getElementsByClassName('.message');
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number';
+// document.querySelector('.number').textContent = 7;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 17;
+// console.log(document.querySelector('.guess').value);
 
 const number = Math.trunc(Math.random() * 20) + 1;
 
